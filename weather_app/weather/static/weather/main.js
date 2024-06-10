6d379f765b356428c11b1b0a13c3c202b699a126
SinxRofozoteron <alex.burakou@gmail.com>
2024-06-10 12:14:05 -0600

    Finished with form styles

--- weather_app/weather/static/weather/main.js
+++ weather_app/weather/static/weather/main.js
@@ -1,5 +1,5 @@
 const cityInputEl = document.getElementById("city");
-const inputContainer = document.querySelector("form > div");
+const inputContainer = document.querySelector("form > div:first-child");
 const searchButton = document.querySelector("button");
 
 let cityInputTimer;
@@ -19,22 +19,50 @@ const checkCityName = async userInput => {
     }
 }
 
+let currentWeatherEl;
+const renderWeather = ({ temperature, precipitation, wind_speed, city, country }) => {
+    if (currentWeatherEl) {
+        currentWeatherEl.remove();
+    }
+
+    currentWeatherEl = document.createElement("div");
+    currentWeatherEl.innerHTML = `
+        <p id="weather-presntation-label">Current weather in ${city}, ${country}</p>
+        <div class="weather-grid">
+            <div>
+                <p>Temperature</p>
+                <p>${Math.round(temperature)}°C</p>
+            </div>
+            <div>
+                <p>Precipitation</p>
+                <p>${precipitation} mm</p>
+            </div>
+            <div>
+                <p>Wind speed</p>
+                <p>${Math.round(wind_speed)}kmh</p>
+        </div>
+    `
+    document.body.appendChild(currentWeatherEl);
+}
+
 const renderSuggestions = suggestions => {
     elementsStrs = suggestions.map(suggestion => {
         [city, country] = suggestion
         return `<li>${city}, ${country}</li>`
     })
-    helperTextEl.innerHTML = `<p>Possible matches:</p> <ul>${elementsStrs.join("")}</ul>`
-    if (!helperTextEl.isConnected) {
-        inputContainer.insertAdjacentElement('afterend', helperTextEl)
-    }
+    helperTextEl.innerHTML = `<p id="matches-label">Possible matches:</p> <ul aria-labelledby="matches-label">${elementsStrs.join("")}</ul>`;
+    helperTextEl.classList.add("open");
 
     const optionsEl = helperTextEl.querySelector("ul");
     optionsEl.addEventListener('click', e => {
         const userInput = e.target.innerText;
         cityInputEl.value = userInput;
-        getWeather(userInput)
-        helperTextEl.remove()
+        getWeather(userInput).then(response => {
+            response.json().then(data => {
+                renderWeather(data);
+            })
+        })
+        helperTextEl.classList.remove("open");
     })
 }
 
@@ -44,6 +72,8 @@ const getWeather = async userInput => {
 }
 
 let helperTextEl = document.createElement("div");
+helperTextEl.classList.add("drawer");
+inputContainer.appendChild(helperTextEl)
 cityInputEl.addEventListener("input", e => {
     clearTimeout(cityInputTimer);
 
@@ -55,10 +85,12 @@ cityInputEl.addEventListener("input", e => {
                 if (Array.isArray(result)) {
                     renderSuggestions(result)
                 } else {
-                    helperTextEl.remove();
+                    helperTextEl.classList.remove("open");
                 }
             })
         }, 1000)
+    } else {
+        helperTextEl.classList.remove("open");
     }
 })
 
@@ -72,7 +104,7 @@ const getWeatherEventHandler = async (e) => {
     if (response.status === 400) {
         renderSuggestions(data['suggestions']);
     } else if (response.status === 200) {
-        console.log(data)
+        renderWeather(data);
     }
 }
 
